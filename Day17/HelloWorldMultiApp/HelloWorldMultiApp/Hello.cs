91f9495379c65aa61563b6a429a32dee4cd6515a
MMathigit <mathimagal@gmail.com>
2025-02-10 07:53:36 +0530

    test

---
+++ Day17/HelloWorldMultiApp/HelloWorldMultiApp/Hello.cs
@@ -0,0 +1,12 @@
+﻿class Hello
+{
+    static void Main()
+    {   
+        Console.WriteLine("Enter the number of times the test should print");
+        int num = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine("Hello, Welcome");
+        }
+    }
+}
