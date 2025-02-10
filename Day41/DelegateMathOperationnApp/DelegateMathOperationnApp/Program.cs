a19e857ab3b9baaed6cbd5affb2cdee749633947
MMathigit <mathimagal@gmail.com>
2025-02-10 23:22:48 +0530

    1

---
+++ Day41/DelegateMathOperationnApp/DelegateMathOperationnApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DelegateMathOperationnApp
+{
+    delegate void MathOper(int num1, int num2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MultiCasting Delegates");
+            Console.WriteLine("-----------------------");
+            MathOper mo;
+            mo = Add;
+            mo += Sub;
+            mo += Multi;
+            mo += Mod;
+            mo(10, 2);
+        }
+        static void Add(int num1, int num2)
+        {
+            var res = num1 + num2;
+            Console.WriteLine("Addition is " + res);
+        }
+        static void Sub(int num1, int num2)
+        {
+            var res = num1 - num2;
+            Console.WriteLine($"Subtraction is {res}");
+        }
+        static void Multi(int num1, int num2)
+        {
+            var res = num1 * num2;
+            Console.WriteLine("Multiplication is " + res);
+        }
+        static void Mod(int num1, int num2)
+        {
+            var res = num1 / num2;
+            Console.WriteLine("Division is " + res);
+
+        }
+    }
+}
---
+++ Day41/SimpleDelegateApp/SimpleDelegateApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SimpleDelegateApp
+{
+    delegate void DPrintMessage(string name);
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CaseStudy1();
+            CaseStudy2();//multi cast Delegates
+        }
+        private static void CaseStudy1() {
+            DPrintMessage x; //x expects a function
+            x = PrintHello;
+            x("Mathi");
+            x = PrintGoodBye;
+            x("Magal");
+
+
+        }
+        private static void CaseStudy2() {
+            DPrintMessage x;
+            x = PrintGoodBye;
+            x += PrintGoodBye;
+            x += PrintHello;
+            x("Check1");
+           
+                }
+
+        static void PrintHello(string name) {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        static void PrintGoodBye(string name)
+        {
+            Console.WriteLine($"GoodBye {name}");
+        }
+        void foo() { }
+    }
+}
